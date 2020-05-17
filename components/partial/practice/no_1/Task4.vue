9059dd6aaa8b15727663f5ad566fccd6c3effb31
ameise-117 <ameise.dorf@gmail.com>
2020-05-17 18:48:54 +0900

    タスク追加

---
+++ components/partial/practice/no_1/Task4.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+	.task
+		svg(xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="344" height="247" viewBox="0 0 344 247")
+			g(transform="translate(-775 -383)")
+				g(transform="translate(266 -69)")
+					rect(width="300" height="8" transform="translate(529 483)" fill="#dcdcd9")
+				line(y2="17" transform="translate(895.5 410.5)" fill="none" stroke="#4e4e4e" stroke-width="1.5" stroke-dasharray="3")
+				line(y2="17" transform="translate(995.5 410.5)" fill="none" stroke="#4e4e4e" stroke-width="1.5" stroke-dasharray="3")
+				circle(cx="8" cy="8" r="8" transform="translate(1087 410)" fill="#707070")
+				text(transform="translate(1095 440)" fill="#707070" font-size="12" font-family="HiraginoSans-W3, Hiragino Sans")
+					tspan(x="-24" y="0") スタート
+				g(transform="translate(1282 -115) rotate(90)")
+					rect(width="204" height="8" transform="translate(529 483)" fill="#dcdcd9")
+				g(transform="translate(787 614)" fill="#fff" stroke="#d4d4d4" stroke-width="1.5")
+					circle(cx="8" cy="8" r="8" stroke="none")
+					circle(cx="8" cy="8" r="7.25" fill="none")
+				text(transform="translate(826 627)" fill="#707070" font-size="12" font-family="HiraginoSans-W3, Hiragino Sans")
+					tspan(x="-17.58" y="0") ゴール
+				line(y2="17" transform="translate(803 522) rotate(90)" fill="none" stroke="#4e4e4e" stroke-width="1.5" stroke-dasharray="3")
+				image(ref="targetEl" width="40" height="38.621" :transform="startPosition" xlink:href="~/assets/images/practice/target_left.png")
+				rect(ref="routeEl1" width="40" height="38.62" transform="translate(975 383)" fill="none")
+				rect(ref="routeEl2" width="40" height="38.62" transform="translate(875 383)" fill="none")
+				rect(ref="routeEl3" width="40" height="38.62" transform="translate(775 383)" fill="none")
+				rect(ref="routeEl4" width="40" height="38.62" transform="translate(775 483)" fill="none")
+				rect(ref="goalEl" width="40" height="38.62" transform="translate(775 583)" fill="none")
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			checkPoint: 4
+		}
+	},
+	computed: {
+		startPosition() {
+			const x = this.$store.state.startPointX
+			const y = this.$store.state.startPointY
+      return `translate(${x}, ${y})`
+		}
+	},
+  methods: {
+  	setTargetEl() {
+      // 動作対象を設定
+    	this.$store.dispatch('targetEl', this.$refs.targetEl)
+    	this.$store.dispatch('goalEl', this.$refs.goalEl)
+    	this.$store.dispatch('startPointX', 1075)
+    	this.$store.dispatch('startPointY', 383)
+
+    	// チェックポイントを格納
+    	let routeEls = []
+    	for (var i = 1; i <= this.checkPoint; i++) {
+    		routeEls.push(this.$refs['routeEl' + i])
+    	}
+    	this.$store.dispatch('routeEls', routeEls)
+	  }
+  }
+}
+</script>
--- pages/practice/1.vue
+++ pages/practice/1.vue
@@ -3,22 +3,32 @@
     ul.step
       li.item(@click="changeTask(1, $refs.task1)", :class="{ active: (taskNo == 1) }") STEP1
       li.item(@click="changeTask(2, $refs.task2)", :class="{ active: (taskNo == 2) }") STEP2
+      li.item(@click="changeTask(3, $refs.task3)", :class="{ active: (taskNo == 3) }") STEP3
+      li.item(@click="changeTask(4, $refs.task4)", :class="{ active: (taskNo == 4) }") STEP4
     .contents
       transition(:name="slideType")
         Task1(v-show="taskNo == 1", ref="task1")
       transition(:name="slideType")
         Task2(v-show="taskNo == 2", ref="task2")
+      transition(:name="slideType")
+        Task3(v-show="taskNo == 3", ref="task3")
+      transition(:name="slideType")
+        Task4(v-show="taskNo == 4", ref="task4")
     img.complete(src="~/assets/images/practice/complete.gif", :class="{ visible: $store.state.isComplete }")
 </template>
 
 <script>
 import Task1 from '~/components/partial/practice/no_1/Task1.vue'
 import Task2 from '~/components/partial/practice/no_1/Task2.vue'
+import Task3 from '~/components/partial/practice/no_1/Task3.vue'
+import Task4 from '~/components/partial/practice/no_1/Task4.vue'
 
 export default {
 	components: {
     Task1,
-    Task2
+    Task2,
+    Task3,
+    Task4
   },
   data() {
   	return {
@@ -41,9 +51,10 @@ export default {
 
       // 起点設定
       task.setTargetEl()
-
       // タスクNo設定
 	  	this.taskNo = num
+      // 完了フラグ初期状態設定
+      this.$store.dispatch('isComplete', false)
 	  }
   }
 }
