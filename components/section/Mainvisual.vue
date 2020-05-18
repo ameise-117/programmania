810b52fd5784787e0aea7074bbee3fcf1d2ac9fa
ameise-117 <ameise.dorf@gmail.com>
2020-05-19 00:55:55 +0900

    メインビジュアル修正

--- components/section/Mainvisual.vue
+++ components/section/Mainvisual.vue
@@ -1,21 +1,60 @@
 <template lang="pug">
 	.mainvisual
+		h1.title
+			| プログラミング的思考力を育てる
+			br.br
+			| 学習サイト
 		.lead
+			p.text
+				| 画面のコマンドを配置して
+				br.br
+				| ハリネズミを動かそう！
 			img.img(src="~/assets/images/practice/target_center.png")
+		a.button(href="/practice/1") スタート
 </template>
 
 <style scoped>
 .mainvisual {
 	display: flex;
+	flex-direction: column;
 	align-items: center;
 	justify-content: center;
 	flex: 1;
   height: 100%;
+  text-align: center;
+  color: var(--color-key-1);
 }
 
 .lead {
+	margin-top: 40px;
+
 	& .img {
-		width: 100px;
+		margin-top: 5px;
+		width: 70px;
+		height: auto;
+	}
+}
+
+.button {
+	margin-top: 70px;
+	width: 200px;
+	height: 60px;
+	background-color: var(--color-key-4);
+	color: var(--color-text-2);
+	font-size: 22px;
+	font-weight: bold;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	cursor: pointer;
+	border-radius: 30px;
+	border: 2px solid transparent;
+	transition: var(--transition-link);
+
+	&:hover {
+		background-color: var(--color-bg-1);
+		color: var(--color-key-4);
+		border-color: var(--color-key-4);
 	}
 }
 </style>
--- layouts/init.vue
+++ layouts/init.vue
@@ -2,19 +2,16 @@
   div
     globalheader
     main.main
-      navigation
       mainvisual
 </template>
 
 <script>
 import Globalheader from '~/components/global/Globalheader.vue'
-import Navigation from '~/components/section/Navigation.vue'
 import Mainvisual from '~/components/section/Mainvisual.vue'
 
 export default {
   components: {
     Globalheader,
-    Navigation,
     Mainvisual
   }
 }
